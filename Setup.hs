641e43628b8d1d2f7a16fe9f01fc451199a24b8a
Shao Cheng <astrohavoc@gmail.com>
2017-10-13 21:33:50 +0800

    Initial commit

---
+++ Setup.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Main
+  ( main
+  ) where
+
+import Data.Binary
+import Distribution.Simple
+
+main :: IO ()
+main =
+  defaultMainWithHooks $
+  simpleUserHooks
+  { postConf =
+      \args flags pkg_descr lbi -> do
+        encodeFile "mangekyou.lbi.buildinfo" lbi
+        postConf simpleUserHooks args flags pkg_descr lbi
+  }
